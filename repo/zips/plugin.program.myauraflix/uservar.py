93381d461c909e7d6ed8aae4f7e59720a622b576
blry12 <bradl79@comcast.net>
2023-12-28 20:22:33 -0600

    update

---
+++ repo/zips/plugin.program.myauraflix/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://auraflixrepo.000webhostapp.com/myauraflix/texts/Nexus_Builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://auraflixrepo.000webhostapp.com/myauraflix/texts/wizard_notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
